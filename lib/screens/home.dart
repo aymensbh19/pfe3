7a4fb02703b2b1f937c601aeff5ed4476e9aa6a5
aymensbh <49540075+aymensbh@users.noreply.github.com>
2019-06-19 01:49:03 +0100

    First commit

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_chat_app/util/firebasehelper.dart';
+import 'package:flutter_chat_app/widgets/msg.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -11,13 +13,38 @@ class HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home",style: TextStyle(color: Colors.black),),
+        title: Text(
+          "Home",
+          style: TextStyle(color: Colors.black),
+        ),
         centerTitle: true,
         elevation: 0,
         backgroundColor: Colors.white,
       ),
-      body: Center(
-        child: Text("body"),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: firestore
+            .collection("chat")
+            .where("cparts", arrayContains: firebaseUser.uid)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData ||
+              snapshot.connectionState == ConnectionState.none ||
+              snapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: Text("No chats yet",
+                      style: TextStyle(color: Colors.grey)));
+              }else{
+                return ListView.builder(
+                  itemCount: snapshot.data.documents.length,
+                  itemBuilder: (context,position){
+                    return Msg(
+                    doc: snapshot.data.documents[position],
+                  );
+                  },
+                );
+
+              }
+        },
       ),
     );
   }
