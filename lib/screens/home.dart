2ed1d191b7a959e2708cd44b2d0f8f282755b6cd
aymensbh <49540075+aymensbh@users.noreply.github.com>
2019-06-21 14:04:47 +0100

    First commit

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -12,53 +12,48 @@ class HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: InkWell(
-              child: Column(
+      child: Column(
           children: <Widget>[
             Container(
-              alignment: Alignment.centerLeft,
-              margin: EdgeInsets.only(top: 4,left: 12,bottom: 10),
-              height: MediaQuery.of(context).size.width/10,
-              child:Text(
-              "Home",
-              style: TextStyle(fontSize: 24,color: Colors.black),
+      alignment: Alignment.centerLeft,
+      margin: EdgeInsets.only(top: 4,left: 12,bottom: 10),
+      height: MediaQuery.of(context).size.width/10,
+      child:Text(
+      "Home",
+      style: TextStyle(fontSize: 24,color: Colors.black),
             ),
             ),
              
           Expanded(
-                    child: StreamBuilder<QuerySnapshot>(
-              stream: firestore
-                  .collection("chat")
-                  .where("cparts", arrayContains: firebaseUser.uid)
-                  .snapshots(),
-              builder: (context, snapshot) {
-                if (!snapshot.hasData ||
-                    snapshot.connectionState == ConnectionState.none ||
-                    snapshot.connectionState == ConnectionState.waiting ||snapshot.data.documents.length==0) {
-                      return Center(
-                        child: Text("No chats yet",
-                            style: TextStyle(color: Colors.grey)));
-                    }else{
-                      return ListView.builder(
-                        itemCount: snapshot.data.documents.length,
-                        itemBuilder: (context,position){
-                          return Msg(
-                          doc: snapshot.data.documents[position],
-                        );
-                        },
-                      );
+            child: StreamBuilder<QuerySnapshot>(
+      stream: firestore
+          .collection("chat")
+          .where("cparts", arrayContains: firebaseUser.uid)
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData ||
+            snapshot.connectionState == ConnectionState.none ||
+            snapshot.connectionState == ConnectionState.waiting ||snapshot.data.documents.length==0) {
+              return Center(
+                child: Text("No chats yet",
+                    style: TextStyle(color: Colors.grey)));
+            }else{
+              return ListView.builder(
+                itemCount: snapshot.data.documents.length,
+                itemBuilder: (context,position){
+                  return Msg(
+                  doc: snapshot.data.documents[position],
+                );
+                },
+              );
 
-                    }
-              },
+            }
+      },
             ),
           ),
           ],
           
         ),
-
-        onTap: (){},
-        onLongPress: (){},
-      ),
     );
   }
 }
