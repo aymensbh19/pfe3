0fccbebe36512943c80bb8d5e668fce44430d53d
aymensbh <49540075+aymensbh@users.noreply.github.com>
2019-06-21 14:40:48 +0100

    First commit

--- lib/widgets/chat.dart
+++ lib/widgets/chat.dart
@@ -16,12 +16,17 @@ class Chat extends StatefulWidget {
 
 class _ChatState extends State<Chat> {
   String cpartner;
+  TextEditingController _controller;
+  ScrollController _scrollController;
 
   @override
   void initState() {
     cpartner = widget.doc.data["cparts"][0] == firebaseUser.uid
         ? widget.doc.data["cparts"][1].toString()
         : widget.doc.data["cparts"][0].toString();
+        
+    _controller = new TextEditingController();
+    _scrollController = new ScrollController();
 
     super.initState();
   }
@@ -45,6 +50,92 @@ class _ChatState extends State<Chat> {
           },
         )
       ),
+      body: Container(
+                  child: StreamBuilder<QuerySnapshot>(
+                stream: firestore
+                    .collection("Chat")
+                    .document(widget.doc.documentID)
+                    .collection("Message")
+                    .orderBy("mdate", descending: true)
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return Center(
+                      child: CircularProgressIndicator(
+                        strokeWidth: 2,
+                      ),
+                    );
+                  } else {
+                    return ListView.builder(
+                      reverse: true,
+                      controller: _scrollController,
+                      itemCount: snapshot.data.documents.length,
+                      itemBuilder: (context, position) {
+                        if (snapshot.data.documents[position].data["mfrom"] ==
+                            firebaseUser.uid) {
+                          return _msgSent(
+                              snapshot.data.documents[position].data["mctn"]);
+                        } else {
+                          return _msgRecived(
+                              snapshot.data.documents[position].data["mctn"],
+                              snapshot.data.documents[position].data["mctn"]);
+                        }
+                      },
+                    );
+                  }
+                },
+              )),
     );
+  }
+
+  
+  Widget _msgRecived(String mctn, String cimg) {
+    return Container(
+        margin: EdgeInsets.only(right: 100, left: 20),
+        alignment: Alignment.centerLeft,
+        child: Container(
+          decoration: BoxDecoration(
+              boxShadow: <BoxShadow>[
+                BoxShadow(
+                    blurRadius: 5,
+                    color: Color.fromRGBO(20, 20, 20, 1),
+                    offset: Offset(0, 2)),
+              ],
+              color: Colors.grey,
+              borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(20),
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                  bottomLeft: Radius.circular(4))),
+          padding: EdgeInsets.all(8),
+          margin: EdgeInsets.all(4),
+          child:
+              Text(mctn, style: TextStyle(color: Colors.black, fontSize: 16)),
+        ));
+  }
+
+  Widget _msgSent(String mctn) {
+    return Container(
+        margin: EdgeInsets.only(left: 100, right: 20),
+        alignment: Alignment.centerRight,
+        child: Container(
+          decoration: BoxDecoration(
+              boxShadow: <BoxShadow>[
+                BoxShadow(
+                    blurRadius: 5,
+                    color: Color.fromRGBO(20, 20, 20, 1),
+                    offset: Offset(0, 2)),
+              ],
+              color: Colors.purple,
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                  bottomRight: Radius.circular(4))),
+          padding: EdgeInsets.all(8),
+          margin: EdgeInsets.all(4),
+          child:
+              Text(mctn, style: TextStyle(color: Colors.purple, fontSize: 16)),
+        ));
   }
 }
