f1a920ca1d38a77aa0349c1af88b56f50e009d5b
aymensbh <49540075+aymensbh@users.noreply.github.com>
2019-06-21 21:44:32 +0100

    First commit

--- lib/widgets/chat.dart
+++ lib/widgets/chat.dart
@@ -31,160 +31,163 @@ class _ChatState extends State<Chat> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          elevation: 1,
-          title: StreamBuilder<DocumentSnapshot>(
-            stream: firestore.collection("user").document(cpartner).snapshots(),
-            builder: (context, snapshot) {
-              if (!snapshot.hasData ||
-                  snapshot.connectionState == ConnectionState.none ||
-                  snapshot.connectionState == ConnectionState.waiting) {
-                return Text("...");
-              } else {
-                return Text(
-                  snapshot.data["username"],
-                );
-              }
-            },
-          ),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.info_outline),
-              onPressed: () {},
-            )
-          ],
-        ),
-        body: Container(
-          color: Colors.white,
-          child: Column(
-            children: <Widget>[
-              Expanded(
-                child: Container(
-                    child: StreamBuilder<QuerySnapshot>(
-                  stream: firestore
-                      .collection("chat")
-                      .document(widget.doc.documentID)
-                      .collection("message")
-                      .orderBy("mdate", descending: true)
-                      .snapshots(),
-                  builder: (context, snapshot) {
-                    if (!snapshot.hasData) {
-                      return Center(
-                        child: CircularProgressIndicator(
-                          strokeWidth: 2,
-                        ),
-                      );
-                    } else {
-                      return ListView.builder(
-                        reverse: true,
-                        controller: _scrollController,
-                        itemCount: snapshot.data.documents.length,
-                        itemBuilder: (context, position) {
-                          if (snapshot.data.documents[position].data["mfrom"] ==
-                              firebaseUser.uid) {
-                            return _msgSent(
-                                snapshot.data.documents[position].data["mctn"]);
-                          } else {
-                            return _msgRecived(
-                                snapshot.data.documents[position].data["mctn"],
-                                snapshot.data.documents[position].data["mctn"]);
-                          }
-                        },
-                      );
-                    }
-                  },
-                )),
-              ),
-              Container(
-                height: 60,
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.only(left: 8, right: 8),
-                      child: IconButton(
-                        icon: Icon(
-                          LineIcons.image,
-                          color: Color(0xFF880E4F),
-                        ),
-                        onPressed: () {},
-                      ),
-                    ),
-                    Expanded(
-                      child: Container(
-                        child: TextField(
-                          controller: _controller,
-                          cursorWidth: 1,
-                          cursorColor: Color(0xFF880E4F),
-                          style: TextStyle(color: Colors.black
-                              // fontFamily: 'product'
-                              ),
-                          decoration: InputDecoration(
-                              alignLabelWithHint: true,
-                              contentPadding: EdgeInsets.all(
-                                  10), //--------------------------------------------
-                              hintText: 'Aa',
-                              hintStyle: TextStyle(
-                                color: Colors.grey,
-                                // fontFamily: 'product'
-                              ),
-                              fillColor: Colors.white,
-                              focusedBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                    color: Color(0xFF880E4F), width: 1),
-                                borderRadius: BorderRadius.circular(30),
-                              ),
-                              filled: true,
-                              enabledBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                    color: Color(0xFF880E4F), width: 1),
-                                borderRadius: BorderRadius.circular(30),
-                              )),
-                        ),
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(left: 8, right: 8),
-                      child: IconButton(
-                        icon: Icon(
-                          Icons.send,
-                          color: Color(0xFF880E4F),
-                        ),
-                        onPressed: () {
-                          // scrolljump+=50;
-                          // _scrollController.jumpTo(scrolljump);
-                          String msg = _controller.text;
-                          _controller.clear();
-
-                          firestore.runTransaction((transactionHandler) async {
-                            await firestore
-                                .collection("chat")
-                                .document(widget.doc.documentID)
-                                .collection("message")
-                                .add({
-                              "mfrom": firebaseUser.uid,
-                              "mctn": msg,
-                              "mdate": DateTime.now().millisecondsSinceEpoch
-                            });
-                            await firestore
-                                .collection("chat")
-                                .document(widget.doc.documentID)
-                                .updateData({
-                              "clastdate": DateTime.now().millisecondsSinceEpoch
-                            });
-                          });
-                          _scrollController.animateTo(0.0,
-                              duration: Duration(milliseconds: 1000),
-                              curve: Curves.bounceInOut);
-                        },
-                      ),
-                    )
-                  ],
-                ),
+    return Material(
+          elevation: 4,
+          child: Scaffold(
+          appBar: AppBar(
+            elevation: 2,
+            title: StreamBuilder<DocumentSnapshot>(
+              stream: firestore.collection("user").document(cpartner).snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData ||
+                    snapshot.connectionState == ConnectionState.none ||
+                    snapshot.connectionState == ConnectionState.waiting) {
+                  return Text("...");
+                } else {
+                  return Text(
+                    snapshot.data["username"],
+                  );
+                }
+              },
+            ),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.info_outline),
+                onPressed: () {},
               )
             ],
           ),
-        ));
+          body: Container(
+            color: Colors.white,
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                      child: StreamBuilder<QuerySnapshot>(
+                    stream: firestore
+                        .collection("chat")
+                        .document(widget.doc.documentID)
+                        .collection("message")
+                        .orderBy("mdate", descending: true)
+                        .snapshots(),
+                    builder: (context, snapshot) {
+                      if (!snapshot.hasData) {
+                        return Center(
+                          child: CircularProgressIndicator(
+                            strokeWidth: 2,
+                          ),
+                        );
+                      } else {
+                        return ListView.builder(
+                          reverse: true,
+                          controller: _scrollController,
+                          itemCount: snapshot.data.documents.length,
+                          itemBuilder: (context, position) {
+                            if (snapshot.data.documents[position].data["mfrom"] ==
+                                firebaseUser.uid) {
+                              return _msgSent(
+                                  snapshot.data.documents[position].data["mctn"]);
+                            } else {
+                              return _msgRecived(
+                                  snapshot.data.documents[position].data["mctn"],
+                                  snapshot.data.documents[position].data["mctn"]);
+                            }
+                          },
+                        );
+                      }
+                    },
+                  )),
+                ),
+                Container(
+                  height: 60,
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.only(left: 8, right: 8),
+                        child: IconButton(
+                          icon: Icon(
+                            LineIcons.image,
+                            color: Color(0xFF880E4F),
+                          ),
+                          onPressed: () {},
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          child: TextField(
+                            controller: _controller,
+                            cursorWidth: 1,
+                            cursorColor: Color(0xFF880E4F),
+                            style: TextStyle(color: Colors.black
+                                // fontFamily: 'product'
+                                ),
+                            decoration: InputDecoration(
+                                alignLabelWithHint: true,
+                                contentPadding: EdgeInsets.all(
+                                    10), //--------------------------------------------
+                                hintText: 'Aa',
+                                hintStyle: TextStyle(
+                                  color: Colors.grey,
+                                  // fontFamily: 'product'
+                                ),
+                                fillColor: Colors.white,
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: Color(0xFF880E4F), width: 1),
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
+                                filled: true,
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: Color(0xFF880E4F), width: 1),
+                                  borderRadius: BorderRadius.circular(30),
+                                )),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(left: 8, right: 8),
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.send,
+                            color: Color(0xFF880E4F),
+                          ),
+                          onPressed: () {
+                            // scrolljump+=50;
+                            // _scrollController.jumpTo(scrolljump);
+                            String msg = _controller.text;
+                            _controller.clear();
+
+                            firestore.runTransaction((transactionHandler) async {
+                              await firestore
+                                  .collection("chat")
+                                  .document(widget.doc.documentID)
+                                  .collection("message")
+                                  .add({
+                                "mfrom": firebaseUser.uid,
+                                "mctn": msg,
+                                "mdate": DateTime.now().millisecondsSinceEpoch
+                              });
+                              await firestore
+                                  .collection("chat")
+                                  .document(widget.doc.documentID)
+                                  .updateData({
+                                "clastdate": DateTime.now().millisecondsSinceEpoch
+                              });
+                            });
+                            _scrollController.animateTo(0.0,
+                                duration: Duration(milliseconds: 1000),
+                                curve: Curves.bounceInOut);
+                          },
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )),
+    );
   }
 
   Widget _msgRecived(String mctn, String cimg) {
